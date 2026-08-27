4dbb227120477bbd04e29123cc43af3cfe161382
Sruthi16189 <sruthiworks26@gmail.com>
2026-08-27 12:27:44 +0530

    Added login feature

---
+++ Login.java
@@ -0,0 +1,14 @@
+public class Login {
+
+    public static void main(String[] args) {
+
+        String username = "Sruthi";
+        String password = "1234";
+
+        if (username.equals("Sruthi") && password.equals("1234")) {
+            System.out.println("Login Successful");
+        } else {
+            System.out.println("Invalid Login");
+        }
+    }
+}
